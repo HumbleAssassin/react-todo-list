f0c63b9f33b0dc0f874e1ae58d4f708e6b77e2b5
HumbleAssassin <thaparajeev1985@gmail.com>
2021-12-27 18:52:14 +0545

    input component added

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,31 @@ import React, { Component } from "react"
 
 export default class TodoInput extends Component {
    render() {
-      return <div></div>
+      const { item, handleChange, handleSubmit, editItem } = this.props
+      return (
+         <div className="card card-body my-3">
+            <form onSubmit={handleSubmit}>
+               <div className="input-group">
+                  <div className="input-group-text bg-primary text-white">
+                     <span className="fas fa-book"></span>
+                  </div>
+                  <input
+                     type="text"
+                     className="form-control text-capitalize"
+                     placeholder="add todo item"
+                     value={item}
+                     onChange={handleChange}
+                  />
+               </div>
+               <div className="d-grid mt-3">
+                  <button
+                     type="submit"
+                     className="btn btn-primary text-uppercase fw-bold">
+                     add item
+                  </button>
+               </div>
+            </form>
+         </div>
+      )
    }
 }
