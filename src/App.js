79997c540bb2886178b77838720888bd1713894c
HumbleAssassin <thaparajeev1985@gmail.com>
2021-12-27 14:03:17 +0545

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { Component } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import TodoList from "./components/TodoList"
+import TodoInput from "./components/TodoInput"
 
-function App() {
-   return (
-      <div className="App">
-         <TodoList />
-      </div>
-   )
+export default class App extends Component {
+   render() {
+      return (
+         <>
+            <div className="container">
+               <div className="row">
+                  <TodoInput />
+                  <TodoList />
+               </div>
+            </div>
+         </>
+      )
+   }
 }
-
-export default App
